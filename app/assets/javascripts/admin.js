df2221b8cc49e59c9400715fe34f36827ebd067e
Dmitry Lavrov <lavrovdv@gmail.com>
2015-05-20 16:10:38 +0400

    чекбоксы

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -8,4 +8,15 @@
 $(function(){
     $("a[rel~=popover], .has-popover").popover();
     $("a[rel~=tooltip], .has-tooltip").tooltip();
+
+    var check_box_list = $('.element_checkbox');
+    $('#toggle-checkboxes').click(function(){
+       var btn_cb = $(this).find('input[type="checkbox"]');
+       btn_cb.prop('checked', !btn_cb.prop('checked'));
+        check_box_list.prop('checked', btn_cb.prop('checked'));
+
+    }).find('input[type="checkbox"]').click(function(){
+        check_box_list.prop('checked', $(this).prop('checked'));
+    });
+
 });
