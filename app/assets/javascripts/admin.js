98d893ebf460d6e2dbb550d5b213a4836e8db982
Dmitry Lavrov <lavrovdv@gmail.com>
2015-05-22 15:45:14 +0400

    страница теста

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -4,6 +4,7 @@
 //= require twitter/bootstrap
 //= require Sortable
 //= require custom
+//= require tinymce-jquery
 
 $(function(){
     $("a[rel~=popover], .has-popover").popover();
@@ -19,4 +20,164 @@ $(function(){
         check_box_list.prop('checked', $(this).prop('checked'));
     });
 
+
+    init_wysiwyg();
+    init_nested_form();
+    upper_downer();
+    row_index();
 });
+
+function init_wysiwyg() {
+    tinymce.init({
+        selector: ".edit",
+        statusbar: false,
+        menubar: false,
+        toolbar: [
+            "undo redo | styleselect | bold italic | link image | alignleft aligncenter alignright"
+        ]
+    });
+
+    //tinymce.init({
+    //    selector: ".edit_hint",
+    //    statusbar: false,
+    //    menubar: false,
+    //    setup: function (ed) {
+    //        ed.on('init', function(args) {
+    //            var id = ed.id;
+    //            var height = 50;
+    //
+    //            document.getElementById(id + '_ifr').style.height = height + 'px';
+    //        });
+    //    }
+    //});
+}
+
+function init_nested_form(){
+    $('.radio-btn').click(function(){
+        $('input:radio').prop('checked',false);
+        $('.radio_correct').prop('value',false);
+        $(this).prop('checked',true);
+        $('input[name^="' + this.name + '"]').prop('value',true)
+    })
+}
+
+function remove_fields(link) {
+    $(link).prev("input[type=hidden]").val("true");
+    $(link).closest(".fields").hide();
+    row_index();
+    off_on();
+}
+
+function add_fields(link, association, content, table_name) {
+    var new_id = (parseInt($("." + table_name + " tr:visible:last td:first ").text()) + 1);
+    new_id = new_id || 1;
+    var regexp = new RegExp("new_" + association, "g");
+    $('#' + table_name).append(content.replace(regexp, new_id));
+    off_on(table_name);
+}
+
+function time_picker(){
+    $('#datetimepicker').datetimepicker({
+        format: 'LT',
+        locale: 'ru'
+    });
+}
+
+function off_on(table_name){
+    $('.radio-btn').off();
+    $(".edit_hint").off();
+    $(".edit").off();
+    init_wysiwyg();
+    init_nested_form();
+    row_index(table_name);
+}
+
+function row_index(table_name, index) {
+    // это для кнопок удаления и вверз вниз
+    if (table_name == null) {
+        $(".answer_table tr:visible td:first-child").each(function (index) { $(this).text(index + 1) });
+        $(".associate_table tr:visible td:first-child").each(function (index) { $(this).text(index + 1) });
+        $(".task_contents_table tr:visible td:first-child").each(function (index) { $(this).text(index + 1) });
+        $(".answer_table tr:visible .serial_number").each(function (index) { $(this).val(index + 1) });
+        $(".associate_table tr:visible .serial_number").each(function (index) { $(this).val(index + 1) });
+    }
+    // а это для создания строк и вставки в конкретную таблицу
+    else {
+        $("." + table_name + " tr:visible td:first-child").each(function (index) { $(this).text(index + 1) });
+        $("." + table_name + " tr:visible .serial_number").each(function (index) { $(this).val(index + 1) });
+    }
+}
+
+function disable_select(){
+    $('.association_select').change(function () {
+        $('.association_select').each(function () { $('option').show() });
+        $('.association_select option:selected').each(function () {
+            if ($(this).text() != 'Не выбрано') {
+                $('.association_select option[value="' + $(this).val() + '"]').hide();
+            }
+        })
+    });
+}
+function tab_selected(){
+    $('.item').click(function () {
+        $('.item').removeClass('selected');
+        $(this).addClass('selected');
+    })
+}
+
+function tasks_list(){
+    $('#show-q-btns').click(function () {
+        $('#q-btns').toggleClass('hide');
+    });
+}
+
+function algorithm_selects(){
+    $('.algorithm').change(function () {
+        var str = "";
+        $(".algorithm option:selected").each(function () {
+            if ($(this).text() == 'Адаптивный выбор') {
+                $('.adaptive_options').removeClass('hide')
+            }
+            else {
+                $('.adaptive_options').addClass('hide')
+            }
+        });
+        $(".algorithm option:selected").each(function () {
+            if ($(this).text() == 'Ограниченое количество заданий') {
+                $('.limited_options').removeClass('hide')
+            }
+            else {
+                $('.limited_options').addClass('hide')
+            }
+        });
+    });
+}
+
+function timer(){
+    $(".seconds_timer").everyTime(1000, function() {
+        if (parseInt($('.seconds_timer').text()) <= 0) {
+            $('.seconds_timer').text(59);
+            if  (parseInt($('.minutes_timer').text()) <= 0) {
+                $('.minutes_timer').text(59);
+                $('.hours_timer').text(parseInt($('.hours_timer').text()) - 1);
+            }
+            else { $('.minutes_timer').text(parseInt($('.minutes_timer').text()) - 1) }
+        }
+        else { $('.seconds_timer').text(parseInt($('.seconds_timer').text()) - 1) }
+    });
+}
+
+function upper_downer() {
+    $('#answer_body, #associate_body').each(function(index, element){
+        console.log(element);
+        Sortable.create(element, {
+            handler: 'sorter',
+            animation: 150,
+            onSort: function (evt) {
+                row_index();
+            }
+        });
+    });
+};
+
+
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,273 +24,214 @@
 //= require moment/ru
 
 
-
 $(function(){
     $("a[rel~=popover], .has-popover").popover();
     $("a[rel~=tooltip], .has-tooltip").tooltip();
 });
-
-function init_wysiwyg() {
-    tinymce.init({
-        selector: ".edit",
-        statusbar: false,
-        menubar: false
-    });
-
-    tinymce.init({
-        selector: ".edit_hint",
-        statusbar: false,
-        menubar: false,
-        setup: function (ed) {
-            ed.on('init', function(args) {
-                var id = ed.id;
-                var height = 50;
-
-                document.getElementById(id + '_ifr').style.height = height + 'px';
-            });
-        }
-    });
-
-//    $('.edit').editable({
-//        buttons: ['bold', 'italic', 'underline', 'formatBlock', 'sep', 'indent', 'outdent',
-//            'insertOrderedList', 'insertUnorderedList', 'insertImage', 'blockStyle'],
-//        // How many colors on a line.
-//        colorsStep: 6,
-//        inlineMode: true,
-//        alwaysVisible: true,
-//        paragraphy: false,
-//        placeholder: 'Введите вариант ответа'
+//
+//function init_wysiwyg() {
+//    tinymce.init({
+//        selector: ".edit",
+//        statusbar: false,
+//        menubar: false
 //    });
-}
-function init_nested_form(){
-    $('.radio-btn').click(function(){
-        $('input:radio').prop('checked',false);
-        $('.radio_correct').prop('value',false);
-        $(this).prop('checked',true);
-        $('input[name^="' + this.name + '"]').prop('value',true)
-    })
-}
-
-function remove_fields(link) {
-    $(link).prev("input[type=hidden]").val("true");
-    $(link).closest(".fields").hide();
-    row_index();
-    off_on();
-}
-
-function add_fields(link, association, content, table_name) {
-    var new_id = (parseInt($("." + table_name + " tr:visible:last td:first ").text()) + 1);
-    new_id = new_id || 1
-    var regexp = new RegExp("new_" + association, "g")
-    $(link).parent().before($("." + table_name).append(content.replace(regexp, new_id)));
-    off_on(table_name);
-}
-
-function time_picker(){
-    $('#datetimepicker').datetimepicker({
-        format: 'LT',
-        locale: 'ru'
-    });
-}
-
-function off_on(table_name){
-    $('.radio-btn').off();
-    $(".edit_hint").off();
-    $(".edit").off();
-//    $('upper').off();
-//    $('downer').off();
-//    $('lefter').off();
-//    $('righter').off();
-//    upper_downer();
-    init_wysiwyg();
-    init_nested_form();
-    row_index(table_name);
-}
-
-function row_index(table_name, index) {
-    // это для кнопок удаления и вверз вниз
-    if (table_name == null) {
-        $(".answer_table tr:visible td:first-child").each(function (index) {
-            $(this).text(index + 1)
-        })
-        $(".associate_table tr:visible td:first-child").each(function (index) {
-            $(this).text(index + 1)
-        })
-        $(".task_contents_table tr:visible td:first-child").each(function (index) {
-            $(this).text(index + 1)
-        })
-        $(".answer_table tr:visible .serial_number").each(function (index) {
-            $(this).val(index + 1)
-        })
-        $(".associate_table tr:visible .serial_number").each(function (index) {
-            $(this).val(index + 1)
-        })
-    }
-    // а это для создания строк и вставки в конкретную таблицу
-    else {
-        $("." + table_name + " tr:visible td:first-child").each(function (index) {
-            $(this).text(index + 1)
-        })
-        $("." + table_name + " tr:visible .serial_number").each(function (index) {
-            $(this).val(index + 1)
-        })
-    }
-}
-
-function disable_select(){
-    $('.association_select').change(function () {
-        $('.association_select').each(function () {
-            $('option').show()
-        })
-        $('.association_select option:selected').each(function () {
-            if ($(this).text() == 'Не выбрано') {
-            }
-            else {
-                $('.association_select option[value="' + $(this).val() + '"]').hide()
-            }
-        })
-    });
-}
-function tab_selected(){
-    $('.item').click(function () {
-        $('.item').removeClass('selected');
-        $(this).addClass('selected');
-    })
-}
-
-function tasks_list(){
-        $('#show-q-btns').click(function () {
-            $('#q-btns').toggleClass('hide');
-        });
-}
-
-function algorithm_selects(){
-        $('.algorithm').change(function () {
-            var str = "";
-            $(".algorithm option:selected").each(function () {
-                if ($(this).text() == 'Адаптивный выбор') {
-                    $('.adaptive_options').removeClass('hide')
-                }
-                else {
-                    $('.adaptive_options').addClass('hide')
-                }
-            });
-            $(".algorithm option:selected").each(function () {
-                if ($(this).text() == 'Ограниченое количество заданий') {
-                    $('.limited_options').removeClass('hide')
-                }
-                else {
-                    $('.limited_options').addClass('hide')
-                }
-            });
-        });
-}
-
-function timer(){
-    $(".seconds_timer").everyTime(1000, function() {
-        if (parseInt($('.seconds_timer').text()) <= 0) {
-            $('.seconds_timer').text(59);
-            if  (parseInt($('.minutes_timer').text()) <= 0) {
-                $('.minutes_timer').text(59)
-                $('.hours_timer').text(parseInt($('.hours_timer').text()) - 1);
-            }
-            else {
-                $('.minutes_timer').text(parseInt($('.minutes_timer').text()) - 1)
-            }
-        }
-        else {
-            $('.seconds_timer').text(parseInt($('.seconds_timer').text()) - 1)
-        }
-    });
-}
-
-function upper_downer() {
-    var item = document.getElementById('items');
-    var assoc_item = document.getElementById('assoc_items');
-    // если найдено два элемента
-    if (item != null && assoc_item !=null){
-        Sortable.create(item, {
-            handler: 'sorter',
-            animation: 150,
-            onSort: function (evt) {
-                row_index();
-            }
-        });
-        Sortable.create(assoc_item, {
-            handler: 'sorter',
-            animation: 150,
-            onSort: function (evt) {
-                row_index();
-            }
-        });
-    }
-    // если только один из них
-    else if (item != null || assoc_item !=null) {
-        if (item !=null) {
-            Sortable.create(item, {
-                handler: 'sorter',
-                animation: 150,
-                onSort: function (evt) {
-                    row_index();
-                }
-            });
-        }
-        else if (assoc_item != null)
-        {
-            Sortable.create(assoc_item, {
-                handler: 'sorter',
-                animation: 150,
-                onSort: function (evt) {
-                    row_index();
-                }
-            });
-        }
-    }
-    // если ничего нет то false
-    else {
-        return false;
-    }
-//    $('.upper').click(function(eventObject){
-//        var curr_tr = $(this).parent().parent();
-//        var prev_tr = $(curr_tr).prev();
-//        prev_tr.insertAfter(curr_tr);
-//        row_index();
-//    });
-//    $('.downer').click(function(eventObject) {
-//        var curr_tr = $(this).parent().parent();
-//        var next_tr = $(curr_tr).next();
-//        next_tr.insertBefore(curr_tr);
-//        row_index();
-//    });
-//    $(".answer_table").rowSorter({
-//        handler: "td.sorter",
-//        onDragStart: function(tbody, row, new_index, old_index){
-//            $('.answer_table tr td:first-child').css('opacity','0.0')
-//        },
-//        onDrop: function(tbody, row, new_index, old_index) {
-//            row_index();
-//            $('.answer_table tr td:first-child').css('opacity','1')
+//
+//    tinymce.init({
+//        selector: ".edit_hint",
+//        statusbar: false,
+//        menubar: false,
+//        setup: function (ed) {
+//            ed.on('init', function(args) {
+//                var id = ed.id;
+//                var height = 50;
+//
+//                document.getElementById(id + '_ifr').style.height = height + 'px';
+//            });
 //        }
 //    });
-//    $(".associate_table").rowSorter({
-//        handler: "td.sorter",
-//        onDragStart: function(tbody, row, new_index, old_index){
-//            $('.associate_table tr td:first-child').css('opacity','0.0')
-//        },
-//        onDrop: function(tbody, row, new_index, old_index) {
-//            row_index();
-//            $('.associate_table tr td:first-child').css('opacity','1')
+//
+//}
+//function init_nested_form(){
+//    $('.radio-btn').click(function(){
+//        $('input:radio').prop('checked',false);
+//        $('.radio_correct').prop('value',false);
+//        $(this).prop('checked',true);
+//        $('input[name^="' + this.name + '"]').prop('value',true)
+//    })
+//}
+//
+//function remove_fields(link) {
+//    $(link).prev("input[type=hidden]").val("true");
+//    $(link).closest(".fields").hide();
+//    row_index();
+//    off_on();
+//}
+//
+//function add_fields(link, association, content, table_name) {
+//    var new_id = (parseInt($("." + table_name + " tr:visible:last td:first ").text()) + 1);
+//    new_id = new_id || 1;
+//    var regexp = new RegExp("new_" + association, "g");
+//    $(link).parent().before($("." + table_name).append(content.replace(regexp, new_id)));
+//    off_on(table_name);
+//}
+//
+//function time_picker(){
+//    $('#datetimepicker').datetimepicker({
+//        format: 'LT',
+//        locale: 'ru'
+//    });
+//}
+//
+//function off_on(table_name){
+//    $('.radio-btn').off();
+//    $(".edit_hint").off();
+//    $(".edit").off();
+//    init_wysiwyg();
+//    init_nested_form();
+//    row_index(table_name);
+//}
+//
+//function row_index(table_name, index) {
+//    // это для кнопок удаления и вверз вниз
+//    if (table_name == null) {
+//        $(".answer_table tr:visible td:first-child").each(function (index) {
+//            $(this).text(index + 1)
+//        })
+//        $(".associate_table tr:visible td:first-child").each(function (index) {
+//            $(this).text(index + 1)
+//        })
+//        $(".task_contents_table tr:visible td:first-child").each(function (index) {
+//            $(this).text(index + 1)
+//        })
+//        $(".answer_table tr:visible .serial_number").each(function (index) {
+//            $(this).val(index + 1)
+//        })
+//        $(".associate_table tr:visible .serial_number").each(function (index) {
+//            $(this).val(index + 1)
+//        })
+//    }
+//    // а это для создания строк и вставки в конкретную таблицу
+//    else {
+//        $("." + table_name + " tr:visible td:first-child").each(function (index) {
+//            $(this).text(index + 1)
+//        })
+//        $("." + table_name + " tr:visible .serial_number").each(function (index) {
+//            $(this).val(index + 1)
+//        })
+//    }
+//}
+//
+//function disable_select(){
+//    $('.association_select').change(function () {
+//        $('.association_select').each(function () {
+//            $('option').show()
+//        })
+//        $('.association_select option:selected').each(function () {
+//            if ($(this).text() == 'Не выбрано') {
+//            }
+//            else {
+//                $('.association_select option[value="' + $(this).val() + '"]').hide()
+//            }
+//        })
+//    });
+//}
+//function tab_selected(){
+//    $('.item').click(function () {
+//        $('.item').removeClass('selected');
+//        $(this).addClass('selected');
+//    })
+//}
+//
+//function tasks_list(){
+//        $('#show-q-btns').click(function () {
+//            $('#q-btns').toggleClass('hide');
+//        });
+//}
+//
+//function algorithm_selects(){
+//        $('.algorithm').change(function () {
+//            var str = "";
+//            $(".algorithm option:selected").each(function () {
+//                if ($(this).text() == 'Адаптивный выбор') {
+//                    $('.adaptive_options').removeClass('hide')
+//                }
+//                else {
+//                    $('.adaptive_options').addClass('hide')
+//                }
+//            });
+//            $(".algorithm option:selected").each(function () {
+//                if ($(this).text() == 'Ограниченое количество заданий') {
+//                    $('.limited_options').removeClass('hide')
+//                }
+//                else {
+//                    $('.limited_options').addClass('hide')
+//                }
+//            });
+//        });
+//}
+//
+//function timer(){
+//    $(".seconds_timer").everyTime(1000, function() {
+//        if (parseInt($('.seconds_timer').text()) <= 0) {
+//            $('.seconds_timer').text(59);
+//            if  (parseInt($('.minutes_timer').text()) <= 0) {
+//                $('.minutes_timer').text(59)
+//                $('.hours_timer').text(parseInt($('.hours_timer').text()) - 1);
+//            }
+//            else {
+//                $('.minutes_timer').text(parseInt($('.minutes_timer').text()) - 1)
+//            }
+//        }
+//        else {
+//            $('.seconds_timer').text(parseInt($('.seconds_timer').text()) - 1)
 //        }
 //    });
-
-
-
-//    $('.lefter').click(function(eventObject){
-//        var curr_tr = $(this).parent().parent();
-//        curr_tr.insertAfter($('.answer_table tr:visible:last'))
-//    })
-//    $('.righter').click(function(eventObject){
-//        var curr_tr = $(this).parent().parent();
-//        curr_tr.insertAfter($('.associate_table tr:visible:last'))
-//    })
-}
+//}
+//
+//function upper_downer() {
+//    var item = document.getElementById('items');
+//    var assoc_item = document.getElementById('assoc_items');
+//    // если найдено два элемента
+//    if (item != null && assoc_item !=null){
+//        Sortable.create(item, {
+//            handler: 'sorter',
+//            animation: 150,
+//            onSort: function (evt) {
+//                row_index();
+//            }
+//        });
+//        Sortable.create(assoc_item, {
+//            handler: 'sorter',
+//            animation: 150,
+//            onSort: function (evt) {
+//                row_index();
+//            }
+//        });
+//    }
+//    // если только один из них
+//    else if (item != null || assoc_item !=null) {
+//        if (item !=null) {
+//            Sortable.create(item, {
+//                handler: 'sorter',
+//                animation: 150,
+//                onSort: function (evt) {
+//                    row_index();
+//                }
+//            });
+//        }
+//        else if (assoc_item != null)
+//        {
+//            Sortable.create(assoc_item, {
+//                handler: 'sorter',
+//                animation: 150,
+//                onSort: function (evt) {
+//                    row_index();
+//                }
+//            });
+//        }
+//    }
+//    // если ничего нет то false
+//    else {
+//        return false;
+//    }
+//}
