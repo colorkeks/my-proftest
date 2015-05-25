6968d6050f3a51620fd07e84293e9a93907b05e8
Evgeny Strokov <caron@comsoft-corp.ru>
2015-05-25 16:17:57 +0400

    fix

--- db/migrate/20150518052131_add_test_group_to_tests.rb
+++ db/migrate/20150518052131_add_test_group_to_tests.rb
@@ -4,6 +4,7 @@ class AddTestGroupToTests < ActiveRecord::Migration
     add_index :tests, :test_group_id
 
     group = TestGroup.find_or_initialize_by(name: 'Тесты')
+    group.save!
     Test.all.each do |test|
       test.test_group = group
       test.save!
