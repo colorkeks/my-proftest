b94018dfbce474e8bb24fa62ae156c57cfd08395
Dmitry Lavrov <lavrovdv@gmail.com>
2015-06-15 16:48:34 +0400

    Поиск врачей

--- app/models/lpu_dbf.rb
+++ app/models/lpu_dbf.rb
@@ -2,4 +2,10 @@ class LpuDbf < ActiveRecord::Base
   acts_as_copy_target
   belongs_to :doctor_dbf, class_name: 'DoctorDbf', foreign_key: :lpuwork, primary_key: 'lpucode'
   belongs_to :officfun_dbf, class_name: 'OfficfunDbf', foreign_key: :lpuwork, primary_key: 'lpucode'
+
+  default_scope {where('dateend is null')}
+
+  def title
+    self.name_s || self.name
+  end
 end
