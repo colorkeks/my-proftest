67dc7cc1b8b13febcc34e23258e66dafebd1a277
Dmitry Lavrov <lavrovdv@gmail.com>
2015-07-02 15:19:32 +0400

    внесение колличества вопросов и чейнов в эквивалентные группы

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -37,14 +37,12 @@ $(function(){
 function update_algorithm_statistic(){
     var submit_form = $('#ajax_submit');
     var inputs = $('#task_chain_counts').find('input[type="text"]');
-    inputs.keypress(function(e){
-        if([48,49,50,51,52,53,54,55,56,57].indexOf(e.keyCode) >= 0){
-           var _this = this;
-            setTimeout(function(){
-                submit_form.find('[name="'+$(_this).attr('name')+'"]').val($(_this).val());
-                submit_form.submit();
-            },100);
-        }
+    inputs.keyup(function(e){
+       var _this = this;
+        setTimeout(function(){
+            submit_form.find('[name="'+$(_this).attr('name')+'"]').val($(_this).val());
+            submit_form.submit();
+        },100);
     });
 }
 
