77bca735fb97f539642238b73dad0b0aeca245b2
Nepomnyaschih Pavel <uberkekss@rambler.ru>
2015-07-06 10:50:43 +0400

    Merge branch 'development' of github.com:dem/proftest into development
    Conflicts:
    	app/controllers/users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
-
   before_action :set_user, only: [:show, :edit, :update, :destroy, :profile, :modes_history, :pdf, :generate_token]
-
   delegate :can?, :cannot?, :to => :ability
   load_and_authorize_resource
   # GET /users
