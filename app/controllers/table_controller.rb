b4f37d103f58e4f8e43172a0e2419483195ceffc
Nepomnyaschih Pavel <uberkekss@rambler.ru>
2015-07-21 13:59:29 +0400

    fix

--- app/controllers/table_controller.rb
+++ app/controllers/table_controller.rb
@@ -1,11 +1,11 @@
 class TableController < ApplicationController
   def index
-    if current_user && current_user.role?('Методолог')
-      redirect_to :test_groups
-    elsif current_user && current_user.role?('Регистратор')
-      redirect_to :doctors
-    elsif current_user && current_user.role?('Тестируемый')
-      redirect_to testee_tab_users_path
-    end
+    # if current_user && current_user.role?('Регистратор')
+    #   redirect_to :doctors
+    # elsif current_user && current_user.role?('Методолог')
+    #   redirect_to :test_groups
+    # elsif current_user && current_user.role?('Тестируемый')
+    #   redirect_to testee_tab_users_path
+    # end
   end
 end
