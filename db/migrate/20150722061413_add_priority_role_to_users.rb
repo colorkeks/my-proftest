1f49abb89fb6fa920b62edf09a9ef287c41ff627
Nepomnyaschih Pavel <uberkekss@rambler.ru>
2015-07-22 11:20:51 +0400

    fix

--- db/migrate/20150722061413_add_priority_role_to_users.rb
+++ db/migrate/20150722061413_add_priority_role_to_users.rb
@@ -1,12 +1,5 @@
 class AddPriorityRoleToUsers < ActiveRecord::Migration
   def change
     add_column :users, :priority_role_id, :string
-    User.reset_column_information
-    User.all.each do |user|
-      if user.priority_role.blank?
-        user.priority_role_id = user.roles.first.id
-        user.save!
-      end
-    end
   end
 end
---
+++ db/migrate/20150722071925_add_initial_priority_roles.rb
@@ -0,0 +1,10 @@
+class AddInitialPriorityRoles < ActiveRecord::Migration
+  def change
+    User.all.each do |user|
+      if user.priority_role.blank?
+        user.priority_role_id = user.roles.first.id
+        user.save!
+      end
+    end
+  end
+end
