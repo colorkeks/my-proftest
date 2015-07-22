1acb5ed699947dac33fb4f6a9a96c337095d0846
Nepomnyaschih Pavel <uberkekss@rambler.ru>
2015-07-22 11:54:21 +0400

    Добавление приоритетной роли. Смена главной страницы в зависимости от приоритетной роли.

--- app/controllers/table_controller.rb
+++ app/controllers/table_controller.rb
@@ -1,10 +1,12 @@
 class TableController < ApplicationController
   def index
-      if current_user && current_user.roles.find(current_user.priority_role_id) == 'Регистратор'
+      if current_user && current_user.roles.find(current_user.priority_role_id).name == 'Администратор'
         redirect_to :doctors
-      elsif current_user && current_user.roles.find(current_user.priority_role_id) == 'Методолог'
+      elsif current_user && current_user.roles.find(current_user.priority_role_id).name == 'Регистратор'
+        redirect_to :doctors
+      elsif current_user && current_user.roles.find(current_user.priority_role_id).name == 'Методолог'
         redirect_to :test_groups
-      elsif current_user && current_user.roles.find(current_user.priority_role_id) == 'Тестируемый'
+      elsif current_user && current_user.roles.find(current_user.priority_role_id).name == 'Тестируемый'
         redirect_to testee_tab_users_path
       end
   end
